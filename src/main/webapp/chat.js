2feefc20d055e52ef79cc5436dd4d5257072d35b
Igor Komissarov <igorkom1990@gmail.com>
2016-12-09 23:04:15 +0300

    chat app: show your own messages too.

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -1,58 +1,51 @@
-new PublishForm(document.forms.publish, 'publish');
-new SubscribePane(document.getElementById('subscribe'), 'subscribe');
+var form = document.forms.publish;
+var chat = document.getElementById('subscribe');
 
-// Посылка запросов -- обычными XHR POST
-function PublishForm(form, url) {
+form.onsubmit = function () {
+    var message = form.message.value;
+    if (message) {
+        form.message.value = '';
+        sendMessage(message);
+    }
+    return false;
+};
 
-  function sendMessage(message) {
+subscribe();
+
+function subscribe() {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+
+        console.log(this);
+        if (this.status == 200) {
+            showMessage(this.responseText);
+            subscribe();
+            return;
+        }
+
+        if (this.status != 404) { // 404 может означать, что сервер перезагружается
+            showMessage(this.statusText); // показать ошибку
+        }
+
+        setTimeout(subscribe, 1000); // попробовать ещё раз через 1 сек
+    };
+    xhr.open("GET", 'subscribe', true);
+    xhr.send();
+}
+
+function sendMessage(message) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'publish', true);
     // просто отсылаю сообщение "как есть" без кодировки
     // если бы было много данных, то нужно было бы отослать JSON из объекта с ними
     // или закодировать их как-то иначе
     xhr.send(message);
-  }
-
-  form.onsubmit = function() {
-    var message = form.message.value;
-    if (message) {
-      form.message.value = '';
-      sendMessage(message);
-    }
-    return false;
-  };
+    showMessage(message);
 }
 
-// Получение сообщений, COMET
-function SubscribePane(elem, url) {
-
-  function showMessage(message) {
+function showMessage(message) {
     var messageElem = document.createElement('div');
     messageElem.appendChild(document.createTextNode(message));
-    elem.appendChild(messageElem);
-  }
-
-  function subscribe() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (this.readyState != 4) return;
-
-      console.log(this);
-      if (this.status == 200) {
-        showMessage(this.responseText);
-        subscribe();
-        return;
-      }
-
-      if (this.status != 404) { // 404 может означать, что сервер перезагружается
-        showMessage(this.statusText); // показать ошибку
-      }
-
-      setTimeout(subscribe, 1000); // попробовать ещё раз через 1 сек
-    }
-    xhr.open("GET", url, true);
-    xhr.send();
-  }
-
-  subscribe();
+    chat.appendChild(messageElem);
 }
