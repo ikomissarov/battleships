9faa211dee49c509a0fa32db6876b99d1a5851c8
Igor Komissarov <igorkom1990@gmail.com>
2016-12-18 17:21:13 +0300

    add bootstrap & jQuery.

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -1,98 +1,80 @@
-var errorCount = 0;
+$(document).ready(function () {
 
-var form = document.forms.publish;
-var chat = document.getElementById('subscribe');
+    $(document).ajaxSuccess(function (event, xhr, settings, data) {
+        console.log(settings.url, ":", data);
+    });
 
-form.onsubmit = function () {
-    var message = form.message.value;
-    if (message) {
-        form.message.value = '';
-        sendMessage(message);
-    }
-    return false;
-};
+    $(document).ajaxError(function (event, xhr, settings) {
+        console.error(settings.url, ":", xhr);
+        showMessage("<b>Error:</b> " + xhr.status + " - " + xhr.statusText, "red");
+    });
 
-// window.onunload = function () {
-//     var xhr = new XMLHttpRequest();
-//     //false param means that request is NOT async as usually so browser will not close before sending it
-//     xhr.open("POST", 'publish', false);
-//     xhr.send("POISON_MSG");
-// };
-
-subscribe();
-
-function subscribe() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState != 4) return;
-
-        console.log(this);
-        if (this.status == 200) {
-            errorCount = 0;
-            var msg = JSON.parse(this.responseText);
-            switch (msg.type) {
-                case "MSG":
-                    showMessage("<b>" + msg.userName + ":</b> " + msg.text, "green");
-                    subscribe();
-                    break;
-                case "NO_MSG":
-                    console.log("No new messages.");
-                    subscribe();
-                    break;
-                case "QUIT":
-                    showMessage(msg.userName + " left the chat.", "red");
-                    break;
-                case "REDIRECT":
-                    window.open(msg.text, "_self");
-                    break;
-                default:
-                    showMessage("<b>Error:</b> " + msg.text, "red");
+    $('#publish').submit(function () {
+        $('#msg').val(function (i, message) {
+            if (message) {
+                sendMessage(message);
+                return '';
             }
-            return;
-        }
+            return message;
+        });
+        return false;
+    });
 
-        showMessage("<b>Error:</b> " + this.status + " - " + this.statusText, "red");
-
-        if (++errorCount <= 10) {
-            setTimeout(subscribe, 1000); // попробовать ещё раз через 1 сек
+    subscribe.errorCount = 0;
+    subscribe.onError = function () {
+        if (++subscribe.errorCount <= 10) {
+            setTimeout(subscribe, 1000);
         } else {
             showMessage("Disconnected from chat.", "red");
         }
     };
-    xhr.open("GET", 'subscribe', true);
-    xhr.send();
+    subscribe();
+});
+
+function subscribe() {
+    $.getJSON('subscribe')
+        .done(function (data) {
+            switch (data.type) {
+                case "MSG":
+                    subscribe.errorCount = 0;
+                    showMessage("<b>" + data.userName + ":</b> " + data.text, "green");
+                    subscribe();
+                    break;
+                case "NO_MSG":
+                    subscribe.errorCount = 0;
+                    subscribe();
+                    break;
+                case "QUIT":
+                    showMessage(data.userName + " left the chat.", "red");
+                    break;
+                case "REDIRECT":
+                    window.open(data.text, "_self");
+                    break;
+                default:
+                    showMessage("<b>Error:</b> " + data.text, "red");
+                    subscribe.onError();
+            }
+        })
+        .fail(subscribe.onError);
 }
 
 function sendMessage(message) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState != 4) return;
-
-        console.log(this);
-        if (this.status == 200) {
-            var msg = JSON.parse(this.responseText);
-            switch (msg.type) {
+    $.post('publish', message)
+        .done(function (data) {
+            switch (data.type) {
                 case "MSG":
-                    showMessage("<b>" + msg.userName + ":</b> " + msg.text, "blue");
+                    showMessage("<b>" + data.userName + ":</b> " + data.text, "blue");
                     break;
                 case "REDIRECT":
-                    window.open(msg.text, "_self");
+                    window.open(data.text, "_self");
                     break;
                 default:
-                    showMessage("<b>Error:</b> " + msg.text, "red");
+                    showMessage("<b>Error:</b> " + data.text, "red");
             }
-            return;
-        }
-
-        showMessage("<b>Error:</b> " + this.status + " - " + this.statusText, "red");
-    };
-    xhr.open("POST", 'publish', true);
-    xhr.send(message);
+        });
 }
 
 function showMessage(message, className) {
-    var messageElem = document.createElement('div');
-    messageElem.className = className;
-    messageElem.innerHTML = message;
-    chat.appendChild(messageElem);
+    var messageElem = $('<div></div>').html(message).addClass(className);
+    $('#subscribe').append(messageElem);
 }
