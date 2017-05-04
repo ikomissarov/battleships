4cd3d2b4c77aa3f3f2d728901d2accaed105c0ea
Igor Komissarov <igorkom1990@gmail.com>
2017-05-04 19:22:37 +0500

    add chat to the game page.

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -1,23 +1,31 @@
 $(document).ready(function () {
 
-    $(document).ajaxSuccess(function (event, xhr, settings, data) {
-        console.log(settings.url, ":", data);
+    var myMsgProtoElem = $('.chat .message:nth-child(1)');
+    var hisMsgProtoElem = $('.chat .message:nth-child(2)');
+
+    $('#open-chat').click(function () {
+        $('#chat').show('fast');
+        return false;
     });
 
-    $(document).ajaxError(function (event, xhr, settings) {
-        console.error(settings.url, ":", xhr);
-        showMessage("<b>Error:</b> " + xhr.status + " - " + xhr.statusText, "red");
+    $('#close-chat').click(function () {
+        $('#chat').hide('fast');
     });
 
-    $('#publish').submit(function () {
-        $('#msg').val(function (i, message) {
+    $('#chat-send').click(function () {
+        $('#chat-msg').val(function (i, message) {
             if (message) {
                 sendMessage(message);
                 return '';
             }
             return message;
         });
-        return false;
+    });
+
+    $('#chat-msg').keypress(function (event) {
+        if (event.which === 13) { //send message on Enter pressed
+            $('#chat-send').click();
+        }
     });
 
     subscribe.errorCount = 0;
@@ -25,59 +33,74 @@ $(document).ready(function () {
         if (++subscribe.errorCount <= 10) {
             setTimeout(subscribe, 1000);
         } else {
-            showMessage("Disconnected from chat.", "red");
+            showError("Disconnected from chat.");
         }
     };
     subscribe();
-});
 
-function subscribe() {
-    $.getJSON('chat/subscribe')
-        .done(function (data) {
+    function subscribe() {
+        $.getJSON('chat/subscribe')
+            .done(function (data) {
+                switch (data.type) {
+                    case "MSG":
+                        subscribe.errorCount = 0;
+                        showMessage(data.userName, data.text, hisMsgProtoElem);
+                        subscribe();
+                        break;
+                    case "NO_MSG":
+                        subscribe.errorCount = 0;
+                        subscribe();
+                        break;
+                    case "QUIT":
+                        showError(data.userName + " left the chat.");
+                        break;
+                    case "REDIRECT":
+                        window.open(data.text, "_self");
+                        break;
+                    default:
+                        showError("<b>Error:</b> " + data.text);
+                        subscribe.onError();
+                }
+            })
+            .fail(subscribe.onError);
+    }
+
+    function sendMessage(message) {
+        $.post({
+            url: 'chat/publish',
+            data: message,
+            contentType: "text/plain;charset=utf-8"
+        }).done(function (data) {
             switch (data.type) {
                 case "MSG":
-                    subscribe.errorCount = 0;
-                    showMessage("<b>" + data.userName + ":</b> " + data.text, "green");
-                    subscribe();
-                    break;
-                case "NO_MSG":
-                    subscribe.errorCount = 0;
-                    subscribe();
-                    break;
-                case "QUIT":
-                    showMessage(data.userName + " left the chat.", "red");
+                    showMessage(data.userName, data.text, myMsgProtoElem);
                     break;
                 case "REDIRECT":
                     window.open(data.text, "_self");
                     break;
                 default:
-                    showMessage("<b>Error:</b> " + data.text, "red");
-                    subscribe.onError();
-            }
-        })
-        .fail(subscribe.onError);
-}
-
-function sendMessage(message) {
-    $.post({
-        url: 'chat/publish',
-        data: message,
-        contentType: "text/plain;charset=utf-8"
-    }).done(function (data) {
-            switch (data.type) {
-                case "MSG":
-                    showMessage("<b>" + data.userName + ":</b> " + data.text, "blue");
-                    break;
-                case "REDIRECT":
-                    window.open(data.text, "_self");
-                    break;
-                default:
-                    showMessage("<b>Error:</b> " + data.text, "red");
+                    showError("<b>Error:</b> " + data.text);
             }
         });
-}
+    }
 
-function showMessage(message, className) {
-    var messageElem = $('<div></div>').html(message).addClass(className);
-    $('#subscribe').append(messageElem);
-}
+    function showMessage(nickname, message, protoElem) {
+        var newElem = protoElem.clone();
+        newElem.find('.nickname').text(nickname);
+        newElem.find('.text').text(message);
+        newElem.find('.time').append(timeNow());
+        $('#chat-body').append(newElem);
+    }
+
+    function showError(message) {
+        var errorElem = $('<li></li>').html(message).addClass('error');
+        $('#chat-body').append(errorElem);
+    }
+
+    function timeNow() {
+        var d = new Date();
+        var h = (d.getHours() < 10 ? '0' : '') + d.getHours();
+        var m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+        return h + ':' + m;
+    }
+});
